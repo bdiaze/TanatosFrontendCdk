425a20272e9b09856423434af14b21ce5164634d
Benjamín <benjamin.diaze@outlook.com>
2025-11-29 23:50:40 -0300

    Se añade esqueleto vacio de environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    cognitoService: {
+        userPoolId: '',
+        clientId: '',
+        baseUrl: '',
+        redirectUrl: '',
+    },
+    tanatosService: {
+        apiUrl: '',
+    },
+};
