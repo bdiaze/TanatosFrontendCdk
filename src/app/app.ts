f28d6659f3334855610d1e44a71a690ca171c282
Benjamín <benjamin.diaze@outlook.com>
2025-11-30 17:07:29 -0300

    Se añade tailwind, footer, header y hero básicos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Hero } from './hero/hero';
+import { Footer } from './footer/footer';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, Header, Hero, Footer],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('tanatos-frontend');
+    protected readonly title = signal('tanatos-frontend');
 }
