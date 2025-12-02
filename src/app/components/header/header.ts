d214dcb58bd01102b2d094b0e94bd98a1005b63e
Benjamín <benjamin.diaze@outlook.com>
2025-12-02 16:07:16 -0300

    Se quita referencia

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,5 @@
 import { AuthStore } from '@services/auth-store';
 import { Component, inject } from '@angular/core';
-import { NgIf } from 'D:/Users/Benjam\u00EDn D\u00EDaz/Documents/Proyectos Angular/tanatos-frontend/node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
     selector: 'app-header',
