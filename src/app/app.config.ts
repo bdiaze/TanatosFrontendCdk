e526935b3104379a04d9ff3c0fb51b98703a946d
Benjamín <benjamin.diaze@outlook.com>
2025-12-05 23:24:47 -0300

    Se añade locales es-CL

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth-interceptor';
 
+import '@/app/helpers/locales';
+
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes),
         provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+        { provide: LOCALE_ID, useValue: 'es-CL' },
     ],
 };
---
+++ src/app/helpers/locales.ts
@@ -0,0 +1,4 @@
+import { registerLocaleData } from '@angular/common';
+import localeEsCL from '@angular/common/locales/es-CL';
+
+registerLocaleData(localeEsCL);
