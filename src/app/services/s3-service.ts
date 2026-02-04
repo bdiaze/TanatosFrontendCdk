2dbc2c14e0c25f17b5f711dddf05dabdfdaf34bf
Benjamín <benjamin.diaze@outlook.com>
2026-02-04 16:07:32 -0300

    Se cambia httpclient por xhr client en subida de archivo a s3, además se corrige bajada de archivos desde s3.

--- src/app/services/s3-service.ts
+++ src/app/services/s3-service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { XhrHttpClient } from './xhr-http-client';
 
 @Injectable({
     providedIn: 'root',
 })
 export class S3Service {
-    constructor(private http: HttpClient) {}
+    constructor(private xhrHttp: XhrHttpClient) {}
 
     subirArchivo(presignedUrl: string, file: File): Observable<HttpEvent<Object>> {
-        return this.http.put(presignedUrl, file, {
+        return this.xhrHttp.put(presignedUrl, file, {
             headers: {
                 'Content-Type': file.type,
             },
@@ -18,10 +19,7 @@ export class S3Service {
         });
     }
 
-    bajarArchivo(presignedUrl: string, filename: string) {
-        const a = document.createElement('a');
-        a.href = presignedUrl;
-        a.download = filename;
-        a.click();
+    bajarArchivo(presignedUrl: string) {
+        window.location.href = presignedUrl;
     }
 }
