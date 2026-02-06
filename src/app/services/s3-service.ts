a45036222019c169fe5b9a3a89aff00b2597690e
Benjamín <benjamin.diaze@outlook.com>
2026-02-05 22:15:14 -0300

    Se añade retry en subida de documento a s3

--- src/app/services/s3-service.ts
+++ src/app/services/s3-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { mergeMap, Observable, retry, retryWhen, throwError, timer } from 'rxjs';
 import { XhrHttpClient } from './xhr-http-client';
 
 @Injectable({
@@ -9,14 +9,28 @@ import { XhrHttpClient } from './xhr-http-client';
 export class S3Service {
     constructor(private xhrHttp: XhrHttpClient) {}
 
-    subirArchivo(presignedUrl: string, file: File): Observable<HttpEvent<Object>> {
-        return this.xhrHttp.put(presignedUrl, file, {
-            headers: {
-                'Content-Type': file.type,
-            },
-            reportProgress: true,
-            observe: 'events',
-        });
+    subirArchivo(presignedUrl: string, file: File): Observable<HttpEvent<void>> {
+        return this.xhrHttp
+            .put<void>(presignedUrl, file, {
+                headers: {
+                    'Content-Type': file.type,
+                },
+                reportProgress: true,
+                observe: 'events',
+            })
+            .pipe(
+                retry({
+                    count: 3,
+                    delay: (error, retryCount) => {
+                        if (error.status === 403 || error.status === 400) {
+                            return throwError(() => error);
+                        }
+
+                        // ⏱️ Backoff simple
+                        return timer(1000 * retryCount);
+                    },
+                }),
+            );
     }
 
     bajarArchivo(presignedUrl: string) {
