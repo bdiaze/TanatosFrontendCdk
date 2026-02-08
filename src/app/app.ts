10547fabea3c2854cb82fbc76f9143e414306c37
Benjamín <benjamin.diaze@outlook.com>
2026-02-08 17:26:01 -0300

    Se añade lógica para scroll to top cuando se cambia de pantalla.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Header } from '@components/header/header';
 import { Footer } from '@components/footer/footer';
+import { filter } from 'rxjs';
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, Header, Footer],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
-export class App {
+export class App implements AfterViewInit {
     protected readonly title = signal('tanatos-frontend');
+
+    @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLElement>;
+
+    private router = inject(Router);
+
+    ngAfterViewInit(): void {
+        this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
+            this.scrollContainer.nativeElement.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        });
+    }
 }
