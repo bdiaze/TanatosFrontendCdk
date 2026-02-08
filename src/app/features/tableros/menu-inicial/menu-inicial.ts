f0dbeb88498edee585a2282d7d0342bac7824eb8
Benjamín <benjamin.diaze@outlook.com>
2026-02-08 13:43:47 -0300

    Se añade nombre de negocio en menu inicial.

--- src/app/features/tableros/menu-inicial/menu-inicial.ts
+++ src/app/features/tableros/menu-inicial/menu-inicial.ts
@@ -1,14 +1,27 @@
 import { environment } from '@/environments/environment';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { HlmItemImports } from '@spartan-ng/helm/item';
 import { HlmH1, HlmH4, HlmP } from '@spartan-ng/helm/typography';
 import { RouterLink } from '@angular/router';
+import { NegocioStore } from '@/app/services/negocio-store';
+import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
+import { HlmSkeletonImports } from '@spartan-ng/helm/skeleton';
 
 @Component({
     selector: 'app-menu-inicial',
-    imports: [HlmH1, HlmH4, HlmP, NgIcon, HlmIcon, HlmItemImports, RouterLink],
+    imports: [
+        HlmH1,
+        HlmH4,
+        HlmP,
+        NgIcon,
+        HlmIcon,
+        HlmItemImports,
+        RouterLink,
+        HlmSpinnerImports,
+        HlmSkeletonImports,
+    ],
     templateUrl: './menu-inicial.html',
     styleUrl: './menu-inicial.scss',
 })
@@ -17,4 +30,7 @@ export class MenuInicial {
     urlCaracteristicas = `${environment.urlImages}/images/caracteristicas-blanco.png`;
     urlComprobacionLista = `${environment.urlImages}/images/comprobacion-de-lista-blanco.png`;
     urlTienda = `${environment.urlImages}/images/tienda-blanco.png`;
+
+    negocioStore = inject(NegocioStore);
+    negocioSeleccionado = this.negocioStore.negocioSeleccionado;
 }
