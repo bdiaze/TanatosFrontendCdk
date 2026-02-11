7942a09f2dd617eb05667ce469eb0d8d869a6cac
Benjamín <benjamin.diaze@outlook.com>
2026-02-11 13:21:34 -0300

    Se añade primera parte de la página de servicios.

--- src/app/features/tableros/servicios/servicios.ts
+++ src/app/features/tableros/servicios/servicios.ts
@@ -1,11 +1,12 @@
+import { environment } from '@/environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servicios',
-  imports: [],
-  templateUrl: './servicios.html',
-  styleUrl: './servicios.scss',
+    selector: 'app-servicios',
+    imports: [],
+    templateUrl: './servicios.html',
+    styleUrl: './servicios.scss',
 })
 export class Servicios {
-
+    urlFondo1 = `${environment.urlImages}/images/vista-frontal-empleado-masculino-sirviendo-cafe.jpg`;
 }
