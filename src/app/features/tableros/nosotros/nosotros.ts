59c653404147eea9a1418e1f284261a7f11fc6f0
Benjamín <benjamin.diaze@outlook.com>
2026-02-12 14:21:24 -0300

    Se añaden versiones de distinta resolucion para imagenes y videos en "Nosotros"

--- src/app/features/tableros/nosotros/nosotros.ts
+++ src/app/features/tableros/nosotros/nosotros.ts
@@ -1,5 +1,5 @@
 import { environment } from '@/environments/environment';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {
     lucideHeartHandshake,
@@ -34,6 +34,20 @@ import { HlmIcon } from '@spartan-ng/helm/icon';
     ],
 })
 export class Nosotros {
-    urlFondo1 = `${environment.urlImages}/images/latina-morena-posando-en-interiores.jpg`;
-    urlVideo1 = `${environment.urlImages}/videos/mujer-tomando-nota-en-planilla.mp4`;
+    urlFondo1_640 = `${environment.urlImages}/images/latina-morena-posando-en-interiores/640.webp`;
+    urlFondo1_960 = `${environment.urlImages}/images/latina-morena-posando-en-interiores/960.webp`;
+    urlFondo1_1280 = `${environment.urlImages}/images/latina-morena-posando-en-interiores/1280.webp`;
+    urlFondo1_1920 = `${environment.urlImages}/images/latina-morena-posando-en-interiores/1920.webp`;
+
+    @ViewChild('bgVideo') video!: ElementRef<HTMLVideoElement>;
+
+    urlVideo1_Poster = `${environment.urlImages}/videos/mujer-tomando-nota-en-planilla/poster.webp`;
+    urlVideo1_WebM = `${environment.urlImages}/videos/mujer-tomando-nota-en-planilla/720p.webm`;
+    urlVideo1_MP4 = `${environment.urlImages}/videos/mujer-tomando-nota-en-planilla/720p.mp4`;
+
+    ngAfterViewInit(): void {
+        const vid = this.video.nativeElement;
+        vid.muted = true;
+        vid.play().catch(() => {});
+    }
 }
