11770f358b9bbc6eb8e204098c399a2180e6804a
Benjamín <benjamin.diaze@outlook.com>
2026-02-14 19:50:24 -0300

    Se añade lógica para tener abierto el collpasible según la ruta de la página.

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -13,7 +13,8 @@ import {
     Output,
     signal,
 } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterLink, RouterModule } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {
     lucideCalendarCog,
@@ -35,6 +36,7 @@ import { HlmScrollAreaImports } from '@spartan-ng/helm/scroll-area';
 import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 import { HlmSkeletonImports } from '@spartan-ng/helm/skeleton';
 import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
+import { filter, map } from 'rxjs';
 
 @Component({
     selector: 'app-menu',
@@ -75,6 +77,8 @@ export class Menu {
     @Input() estatico = false;
     @Output() onClose = new EventEmitter<void>();
 
+    private router = inject(Router);
+
     authStore = inject(AuthStore);
     negocioStore = inject(NegocioStore);
     negocioDao = inject(NegocioDao);
@@ -97,43 +101,43 @@ export class Menu {
 
         if (this.negocioSeleccionado()) {
             opciones.push({
-                id: crypto.randomUUID(),
+                id: 'group-negocio-seleccionado',
                 tipo: 'group',
                 titulo: this.negocioSeleccionado()?.nombre!,
                 items: [
                     {
-                        id: crypto.randomUUID(),
+                        id: 'group-negocio-seleccionado-item-mi-calendario',
                         tipo: 'item',
                         icon: 'lucideCalendarRange',
                         titulo: 'Mi Calendario',
-                        url: 'mi-calendario',
+                        url: '/mi-calendario',
                     },
                     {
-                        id: crypto.randomUUID(),
+                        id: 'group-negocio-seleccionado-item-configuracion',
                         tipo: 'item',
                         icon: 'lucideSettings',
                         titulo: 'Configuración',
                         items: [
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-negocio-seleccionado-item-configuracion-subitem-mis-obligaciones',
                                 tipo: 'item',
                                 icon: 'lucideCalendarCog',
                                 titulo: 'Mis Obligaciones',
-                                url: 'mis-obligaciones',
+                                url: '/mis-obligaciones',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-negocio-seleccionado-item-configuracion-subitem-plantillas-inscritas',
                                 tipo: 'subitem',
                                 titulo: 'Plantillas Inscritas',
                                 icon: 'lucideClipboardPaste',
-                                url: 'plantillas-inscritas',
+                                url: '/plantillas-inscritas',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-negocio-seleccionado-item-configuracion-subitem-mis-destinatarios',
                                 tipo: 'item',
                                 icon: 'lucideSend',
                                 titulo: 'Mis Destinatarios',
-                                url: 'mis-destinatario',
+                                url: '/mis-destinatario',
                             },
                         ],
                     },
@@ -142,22 +146,22 @@ export class Menu {
         }
 
         opciones.push({
-            id: crypto.randomUUID(),
+            id: 'group-general',
             tipo: 'group',
             titulo: 'General',
             items: [
                 {
-                    id: crypto.randomUUID(),
+                    id: 'group-general-item-configuracion',
                     tipo: 'item',
                     titulo: 'Configuración',
                     icon: 'lucideSettings',
                     items: [
                         {
-                            id: crypto.randomUUID(),
+                            id: 'group-general-item-configuracion-subitem-mis-negocios',
                             tipo: 'subitem',
                             titulo: 'Mis Negocios',
                             icon: 'lucideStore',
-                            url: 'mis-negocios',
+                            url: '/mis-negocios',
                         },
                     ],
                 },
@@ -166,60 +170,60 @@ export class Menu {
 
         if (this.accesoAdmin()) {
             opciones.push({
-                id: crypto.randomUUID(),
+                id: 'group-administracion',
                 tipo: 'group',
                 titulo: 'Administración',
                 items: [
                     {
-                        id: crypto.randomUUID(),
+                        id: 'group-administracion-item-mantenedores',
                         tipo: 'item',
                         titulo: 'Mantenedores',
                         icon: 'lucideSettings',
                         items: [
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-plantillas',
                                 tipo: 'subitem',
                                 titulo: 'Plantillas',
                                 url: '/administracion/mantenedores/template',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-categoria-norma',
                                 tipo: 'subitem',
                                 titulo: 'Categoria Norma',
                                 url: '/administracion/mantenedores/categoria-norma',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-rubro',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Rubro',
                                 url: '/administracion/mantenedores/tipo-rubro',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-actividad',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Actividad',
                                 url: '/administracion/mantenedores/tipo-actividad',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-destinatario',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Destinatario',
                                 url: '/administracion/mantenedores/tipo-receptor-notificacion',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-fiscalizador',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Fiscalizador',
                                 url: '/administracion/mantenedores/tipo-fiscalizador',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-periodicidad',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Periodicidad',
                                 url: '/administracion/mantenedores/tipo-periodicidad',
                             },
                             {
-                                id: crypto.randomUUID(),
+                                id: 'group-administracion-item-mantenedores-subitem-tipo-unidad-tiempo',
                                 tipo: 'subitem',
                                 titulo: 'Tipo Unidad Tiempo',
                                 url: '/administracion/mantenedores/tipo-unidad-tiempo',
@@ -233,6 +237,16 @@ export class Menu {
         return opciones;
     });
 
+    url = toSignal(
+        this.router.events.pipe(
+            filter((e) => e instanceof NavigationEnd),
+            map(() => this.router.url),
+        ),
+        { initialValue: this.router.url },
+    );
+
+    groupsAbiertos = new Set<string>([]);
+
     constructor() {
         effect(() => {
             if (this.sesionIniciada()) {
@@ -240,6 +254,46 @@ export class Menu {
                 this.obtenerInformacionUsuario();
             }
         });
+
+        effect(() => {
+            const url = this.url();
+            const opciones = this.opcionesMenu();
+
+            for (const item of opciones) {
+                if (item.tipo === 'item' && this.tieneUrlHijoActivo(item, url)) {
+                    this.groupsAbiertos.add(item.id);
+                } else if (item.tipo === 'group' && item.items) {
+                    for (const item2 of item.items) {
+                        if (this.tieneUrlHijoActivo(item2, url)) {
+                            this.groupsAbiertos.add(item2.id);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    tieneUrlHijoActivo(opcion: OpcionMenu, url: string) {
+        if (
+            opcion.tipo === 'item' &&
+            opcion.items &&
+            opcion.items.some((subitem) => subitem.url === url)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    itemCollapsibleChange(groupId: string, isOpen: boolean) {
+        if (isOpen) {
+            this.groupsAbiertos.add(groupId);
+        } else {
+            this.groupsAbiertos.delete(groupId);
+        }
+    }
+
+    estaAbierto(groupId: string) {
+        return this.groupsAbiertos.has(groupId);
     }
 
     cambiarNegocio(negocio: SalNegocio) {
