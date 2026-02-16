406b5f52d3ba049fcb5aee66d6f30931c546df4a
Benjamín <benjamin.diaze@outlook.com>
2026-02-16 17:03:46 -0300

    Se crea pantalla de contacto.

--- src/app/features/tableros/contacto/contacto.ts
+++ src/app/features/tableros/contacto/contacto.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormularioContacto } from '@/app/components/formulario-contacto/formulario-contacto';
+import { environment } from '@/environments/environment';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideMailbox } from '@ng-icons/lucide';
 
 @Component({
-  selector: 'app-contacto',
-  imports: [],
-  templateUrl: './contacto.html',
-  styleUrl: './contacto.scss',
+    selector: 'app-contacto',
+    imports: [FormularioContacto, HlmIcon, NgIcon],
+    templateUrl: './contacto.html',
+    styleUrl: './contacto.scss',
+    providers: [
+        provideIcons({
+            lucideMailbox,
+        }),
+    ],
 })
 export class Contacto {
-
+    urlFondo1_640 = `${environment.urlImages}/images/retrato-de-una-mujer-morena-con-gafas-sentada-en-un-cafe/640.webp`;
+    urlFondo1_960 = `${environment.urlImages}/images/retrato-de-una-mujer-morena-con-gafas-sentada-en-un-cafe/960.webp`;
+    urlFondo1_1280 = `${environment.urlImages}/images/retrato-de-una-mujer-morena-con-gafas-sentada-en-un-cafe/1280.webp`;
+    urlFondo1_1920 = `${environment.urlImages}/images/retrato-de-una-mujer-morena-con-gafas-sentada-en-un-cafe/1920.webp`;
 }
