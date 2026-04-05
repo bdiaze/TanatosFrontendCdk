c02471c366cd86012465f5c61d27d90a1d8f80e4
Benjamín <benjamin.diaze@outlook.com>
2026-04-05 18:36:28 -0400

    Se termina lógica para suscripción y cancelación de suscripción en pantalla de "Mi Plan".

--- src/app/features/mantenedores/mantenedor-suscripcion/mantenedor-suscripcion.ts
+++ src/app/features/mantenedores/mantenedor-suscripcion/mantenedor-suscripcion.ts
@@ -1,22 +1,41 @@
+import { ModalEliminacion } from '@/app/components/modal-eliminacion/modal-eliminacion';
 import { PlanDao } from '@/app/daos/plan-dao';
 import { SuscripcionDao } from '@/app/daos/suscripcion-dao';
+import { EntSuscripcionCrear } from '@/app/entities/others/ent-suscripcion-crear';
 import { SalPlan } from '@/app/entities/others/sal-plan';
 import { SalSuscripcion } from '@/app/entities/others/sal-suscripcion';
 import { getErrorMessage } from '@/app/helpers/error-message';
 import { NegocioStore } from '@/app/services/negocio-store';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideCalendarRange, lucideCreditCard, lucideDot, lucideGem } from '@ng-icons/lucide';
+import {
+    lucideCalendarRange,
+    lucideChevronRight,
+    lucideCircleCheck,
+    lucideCircleX,
+    lucideCreditCard,
+    lucideDiamond,
+    lucideDot,
+    lucideGem,
+    lucideHourglass,
+    lucideRefreshCw,
+    lucideRefreshCwOff,
+    lucideUserRound,
+    lucideUsersRound,
+} from '@ng-icons/lucide';
 import { BrnTooltipImports } from '@spartan-ng/brain/tooltip';
 import { HlmAlertImports } from '@spartan-ng/helm/alert';
 import { HlmBadgeImports } from '@spartan-ng/helm/badge';
 import { HlmBreadCrumbImports } from '@spartan-ng/helm/breadcrumb';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { HlmItemImports } from '@spartan-ng/helm/item';
 import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 import { HlmSkeletonImports } from '@spartan-ng/helm/skeleton';
 import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
+import { HlmTableImports } from '@spartan-ng/helm/table';
 import { HlmTooltipImports } from '@spartan-ng/helm/tooltip';
 import { HlmH3, HlmH4, HlmP } from '@spartan-ng/helm/typography';
 
@@ -39,21 +58,35 @@ import { HlmH3, HlmH4, HlmP } from '@spartan-ng/helm/typography';
         HlmTooltipImports,
         HlmSkeletonImports,
         HlmBreadCrumbImports,
+        HlmButtonImports,
+        ModalEliminacion,
+        HlmTableImports,
     ],
     templateUrl: './mantenedor-suscripcion.html',
     styleUrl: './mantenedor-suscripcion.scss',
     providers: [
         provideIcons({
             lucideCreditCard,
+            lucideHourglass,
             lucideGem,
             lucideDot,
+            lucideChevronRight,
+            lucideRefreshCwOff,
+            lucideRefreshCw,
+            lucideCircleCheck,
+            lucideCircleX,
+            lucideUsersRound,
+            lucideUserRound,
         }),
+        DatePipe,
     ],
 })
 export class MantenedorSuscripcion implements OnInit {
     private suscripcionDao: SuscripcionDao = inject(SuscripcionDao);
     private planDao: PlanDao = inject(PlanDao);
     negocioStore: NegocioStore = inject(NegocioStore);
+    private activatedRoute = inject(ActivatedRoute);
+    private datePipe = inject(DatePipe);
 
     suscripciones = signal([] as SalSuscripcion[]);
     planesVigentes = signal([] as SalPlan[]);
@@ -61,7 +94,9 @@ export class MantenedorSuscripcion implements OnInit {
         const suscripciones = this.suscripciones();
 
         // Buscamos la suscripción con mayor expiración...
-        const suscripcionesConFechaExpiracion = suscripciones.filter((s) => s.fechaExpiracion);
+        const suscripcionesConFechaExpiracion = suscripciones.filter(
+            (s) => s.fechaExpiracion && new Date(s.fechaExpiracion) > new Date(),
+        );
         if (suscripcionesConFechaExpiracion.length > 0) {
             const mayorExpiracion = suscripcionesConFechaExpiracion.reduce((max, actual) =>
                 new Date(actual.fechaExpiracion!) > new Date(max.fechaExpiracion!) ? actual : max,
@@ -71,11 +106,19 @@ export class MantenedorSuscripcion implements OnInit {
 
         return null;
     });
+    fechaExpiracionFormateada = computed(() => {
+        if (this.ultimaSuscripcion()) {
+            return this.datePipe.transform(
+                this.ultimaSuscripcion()?.fechaExpiracion,
+                "EEEE d 'de' MMMM 'de' yyyy",
+            );
+        }
+        return null;
+    });
     esPlanEmpresa = computed(() => {
         const ultimaSuscripcion = this.ultimaSuscripcion();
         if (
             ultimaSuscripcion &&
-            ultimaSuscripcion.estado === 1 /* Activa */ &&
             ultimaSuscripcion.fechaExpiracion &&
             new Date(ultimaSuscripcion.fechaExpiracion) > new Date()
         ) {
@@ -95,9 +138,18 @@ export class MantenedorSuscripcion implements OnInit {
     cargandoPlanesVigentes = signal(true);
     error = signal('');
 
+    esCallback = signal(false);
+
     ngOnInit(): void {
         this.obtenerSuscripciones();
         this.obtenerPlanesVigentes();
+
+        this.activatedRoute.queryParams.subscribe((params) => {
+            const callback = params['callback'];
+            if (callback == 1) {
+                this.esCallback.set(true);
+            }
+        });
     }
 
     obtenerSuscripciones() {
@@ -140,5 +192,84 @@ export class MantenedorSuscripcion implements OnInit {
             .add(() => {
                 this.cargandoPlanesVigentes.set(false);
             });
+    }
+
+    procesandoPago = signal(false);
+
+    generarUrlPago(idPlan: number) {
+        if (this.procesandoPago()) return;
+
+        this.procesandoPago.set(true);
+        this.suscripcionDao
+            .crear({
+                idPlan: idPlan,
+            } as EntSuscripcionCrear)
+            .subscribe({
+                next: (res) => {
+                    if (res.urlSuscripcion) {
+                        window.location.href = res.urlSuscripcion;
+                    }
+                },
+                error: (err) => {
+                    console.error('Error al generar URL para pago de la suscripción', err);
+                    this.error.set(
+                        getErrorMessage(err) ?? 'Error al generar URL para pago de la suscripción',
+                    );
+                },
+            })
+            .add(() => {
+                this.procesandoPago.set(false);
+            });
+    }
+
+    obtenerEstadoSuscripcion(idEstado: number) {
+        switch (idEstado) {
+            case 1:
+                return 'Activa';
+            case 2:
+                return 'Cancelada';
+            case 3:
+                return 'Expirada';
+            case 4:
+                return 'Pago Pendiente';
+            default:
+                return '';
+        }
+    }
+
+    obtenerRenovacion(idPlan: number) {
+        if (idPlan == 1) return null;
+        return 'Automática';
+    }
+
+    showModalDesuscribirse = signal(false);
+
+    openModalDesuscribirse() {
+        this.showModalDesuscribirse.set(true);
+    }
+
+    closeModalDesuscribirse() {
+        this.showModalDesuscribirse.set(false);
+    }
+
+    procesando = signal(false);
+
+    desuscribirse(idSuscripcion: number) {
+        this.procesando.set(true);
+        this.suscripcionDao
+            .cancelar(idSuscripcion)
+            .subscribe({
+                next: () => {
+                    this.obtenerSuscripciones();
+                },
+                error: (err) => {
+                    console.error('Error al cancelar suscripción', err);
+                    this.error.set(getErrorMessage(err) ?? 'Error al cancelar suscripción');
+                },
+            })
+            .add(() => {
+                this.procesando.set(false);
+            });
+        this.showModalDesuscribirse.set(false);
     }
 }
