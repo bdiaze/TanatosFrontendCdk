9bf926d707a155ddc2edbd6c35ee5ce808b74100
Benjamín <benjamin.diaze@outlook.com>
2026-05-25 17:47:23 -0400

    Se cambia comportamiento y texto de listón beta abierta cuando usuario inició sesión.

--- src/app/components/liston-beta/liston-beta.ts
+++ src/app/components/liston-beta/liston-beta.ts
@@ -1,5 +1,7 @@
 import { redireccionarALogin } from '@/app/features/auth/login/login';
-import { Component } from '@angular/core';
+import { AuthStore } from '@/app/services/auth-store';
+import { NgClass } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucidePartyPopper, lucideThumbsUp } from '@ng-icons/lucide';
 import { HlmIcon } from '@spartan-ng/helm/icon';
@@ -7,12 +9,17 @@ import { HlmP } from '@spartan-ng/helm/typography';
 
 @Component({
     selector: 'app-liston-beta',
-    imports: [HlmP, NgIcon, HlmIcon],
+    imports: [HlmP, NgIcon, HlmIcon, NgClass],
     templateUrl: './liston-beta.html',
     styleUrl: './liston-beta.scss',
     providers: [provideIcons({})],
 })
 export class ListonBeta {
+    private authStore = inject(AuthStore);
+    sesionIniciada = computed(() => {
+        return this.authStore.sesionIniciada() || this.authStore.logoutRunning() || this.authStore.callbackRunning();
+    });
+
     async unirseBeta() {
         await redireccionarALogin(true);
     }
