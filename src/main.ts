576695f496e322d0e13e6eab85d7c282884b403a
Benjamín <benjamin.diaze@outlook.com>
2026-05-30 13:05:56 -0400

    Se quita obtención url desde main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-const url = new URL(window.location.href);
-
 bootstrapApplication(App, appConfig)
     .then(() => {
         document.querySelector('app-root')?.classList.add('app-ready');
