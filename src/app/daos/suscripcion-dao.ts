f8d0005a72e917ccdfe63e5101081998a347fd73
Benjamín <benjamin.diaze@outlook.com>
2026-06-17 22:10:31 -0400

    Se añaden suscripciones a negocio store.

--- src/app/daos/suscripcion-dao.ts
+++ src/app/daos/suscripcion-dao.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { SalSuscripcion } from '../entities/others/sal-suscripcion';
 import { environment } from '@/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { EntSuscripcionCrear } from '../entities/others/ent-suscripcion-crear';
 import { SalSuscripcionCrear } from '../entities/others/sal-suscripcion-crear';
+import { NegocioStore } from '../services/negocio-store';
 
 @Injectable({
     providedIn: 'root',
@@ -12,8 +13,16 @@ import { SalSuscripcionCrear } from '../entities/others/sal-suscripcion-crear';
 export class SuscripcionDao {
     constructor(private readonly http: HttpClient) {}
 
+    negocioStore = inject(NegocioStore);
+
     obtenerVigentes(): Observable<SalSuscripcion[]> {
-        return this.http.get<SalSuscripcion[]>(environment.tanatosService.apiUrl + `/Suscripcion/Vigentes`);
+        return this.http.get<SalSuscripcion[]>(environment.tanatosService.apiUrl + `/Suscripcion/Vigentes`).pipe(
+            tap((v) => {
+                if (!this.arraysIguales(v, this.negocioStore.suscripcionesUsuario())) {
+                    this.negocioStore.suscripcionesUsuario.set(v);
+                }
+            }),
+        );
     }
 
     crear(entrada: EntSuscripcionCrear): Observable<SalSuscripcionCrear> {
@@ -22,5 +31,27 @@ export class SuscripcionDao {
 
     cancelar(idSuscripcion: number): Observable<void> {
         return this.http.delete<void>(environment.tanatosService.apiUrl + `/Suscripcion/${idSuscripcion}`);
+    }
+
+    arraysIguales(a: SalSuscripcion[], b: SalSuscripcion[]): boolean {
+        if (a.length !== b.length) return false;
+
+        const mapA = new Map(a.map((x) => [x.id, x]));
+        return b.every((x) => {
+            const original = mapA.get(x.id);
+            if (!original) return false;
+
+            return (
+                original.idPlan === x.idPlan &&
+                original.nombrePlan === x.nombrePlan &&
+                original.precioPlan === x.precioPlan &&
+                original.duracionMesesPlan === x.duracionMesesPlan &&
+                original.fechaInicio === x.fechaInicio &&
+                original.fechaExpiracion === x.fechaExpiracion &&
+                original.fechaCancelacion === x.fechaCancelacion &&
+                original.estado === x.estado &&
+                original.tieneFlowSubscriptionId === x.tieneFlowSubscriptionId
+            );
+        });
     }
 }
--- src/app/services/negocio-store.ts
+++ src/app/services/negocio-store.ts
@@ -1,7 +1,8 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { SalNegocio } from '../entities/others/sal-negocio';
 import { getCookie } from '../helpers/cookie-helper';
 import { SalNegocioInformacionUsuario } from '../entities/others/sal-negocio-informacion-usuario';
+import { SalSuscripcion } from '../entities/others/sal-suscripcion';
 
 @Injectable({
     providedIn: 'root',
@@ -10,4 +11,31 @@ export class NegocioStore {
     negocioSeleccionado = signal<SalNegocio | null>(null);
     negociosUsuario = signal<SalNegocio[]>([]);
     informacionUsuario = signal<SalNegocioInformacionUsuario | null>(null);
+    suscripcionesUsuario = signal<SalSuscripcion[]>([]);
+
+    suscripcionActualUsuario = computed(() => {
+        const suscripciones = this.suscripcionesUsuario();
+
+        // Buscamos la suscripción con mayor expiración...
+        const suscripcionesConFechaExpiracion = suscripciones.filter((s) => s.fechaExpiracion && new Date(s.fechaExpiracion) > new Date());
+        if (suscripcionesConFechaExpiracion.length > 0) {
+            const mayorExpiracion = suscripcionesConFechaExpiracion.reduce((max, actual) =>
+                new Date(actual.fechaExpiracion!) > new Date(max.fechaExpiracion!) ? actual : max,
+            );
+            if (mayorExpiracion) return mayorExpiracion;
+        }
+
+        return null;
+    });
+
+    suscripcionActualGratuita = computed(() => {
+        const suscripcionActual = this.suscripcionActualUsuario();
+        if (!suscripcionActual) return null;
+
+        if (suscripcionActual.precioPlan === 0) {
+            return suscripcionActual;
+        }
+
+        return null;
+    });
 }
