313b89585be6a68f39a9f8e1ae47f374361c45bd
Benjamín <benjamin.diaze@outlook.com>
2026-06-25 20:02:20 -0400

    Upgrade hlm label.

--- libs/ui/label/src/lib/hlm-label.ts
+++ libs/ui/label/src/lib/hlm-label.ts
@@ -1,27 +1,17 @@
-import { Directive, computed, input } from '@angular/core';
+import { Directive } from '@angular/core';
 import { BrnLabel } from '@spartan-ng/brain/label';
-import { hlm } from '@spartan-ng/helm/utils';
-import type { ClassValue } from 'clsx';
+import { classes } from '@spartan-ng/helm/utils';
 
 @Directive({
 	selector: '[hlmLabel]',
-	hostDirectives: [
-		{
-			directive: BrnLabel,
-			inputs: ['id'],
-		},
-	],
-	host: {
-		'[class]': '_computedClass()',
-	},
+	hostDirectives: [{ directive: BrnLabel, inputs: ['id', 'for'] }],
+	host: { 'data-slot': 'label' },
 })
 export class HlmLabel {
-	public readonly userClass = input<ClassValue>('', { alias: 'class' });
-
-	protected readonly _computedClass = computed(() =>
-		hlm(
-			'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 peer-data-[disabled]:cursor-not-allowed peer-data-[disabled]:opacity-50 has-[[disabled]]:cursor-not-allowed has-[[disabled]]:opacity-50',
-			this.userClass(),
-		),
-	);
+	constructor() {
+		classes(
+			() =>
+				'gap-2 text-sm leading-none font-medium group-data-[disabled=true]:opacity-50 peer-disabled:opacity-50 flex items-center select-none group-data-[disabled=true]:pointer-events-none peer-disabled:cursor-not-allowed',
+		);
+	}
 }
