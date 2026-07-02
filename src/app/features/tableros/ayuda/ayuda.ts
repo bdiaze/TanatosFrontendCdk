8ba9218fdd3cb272adffb79ecdaabd0f93f1056e
Benjamín <benjamin.diaze@outlook.com>
2026-07-01 23:14:06 -0400

    Se añaden videos tutoriales en pantalla de ayuda.

--- src/app/features/tableros/ayuda/ayuda.ts
+++ src/app/features/tableros/ayuda/ayuda.ts
@@ -1,10 +1,10 @@
 import { PreguntaFrecuenteDao } from '@/app/daos/pregunta-frecuente-dao';
 import { SalPreguntaFrecuenteHabilitado } from '@/app/entities/others/sal-pregunta-frecuente-habilitado';
 import { getErrorMessage } from '@/app/helpers/error-message';
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnDestroy, OnInit, signal, untracked } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideCircleQuestionMark } from '@ng-icons/lucide';
+import { lucideCircleQuestionMark, lucideX } from '@ng-icons/lucide';
 import { HlmAccordionImports } from '@spartan-ng/helm/accordion';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { HlmH3, HlmH4, HlmP } from '@spartan-ng/helm/typography';
@@ -16,26 +16,50 @@ import { TourService } from '@/app/helpers/tour-service';
 import { DriveStep } from 'driver.js';
 import { MenuHelper } from '@/app/helpers/menu-helper';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { VideoTutorialDao } from '@/app/daos/video-tutorial-dao';
+import { SalVideoTutorialHabilitado } from '@/app/entities/others/sal-video-tutorial-habilitado';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-ayuda',
     imports: [HlmP, HlmH3, HlmH4, NgIcon, HlmIcon, HlmAccordionImports, EditorTexto, HlmSkeletonImports, HlmButtonImports],
     templateUrl: './ayuda.html',
     styleUrl: './ayuda.scss',
-    providers: [provideIcons({ lucideCircleQuestionMark })],
+    providers: [provideIcons({ lucideCircleQuestionMark, lucideX })],
 })
-export class Ayuda implements OnInit {
+export class Ayuda implements OnInit, OnDestroy {
     private readonly destroyRef = inject(DestroyRef);
     private readonly router = inject(Router);
     private readonly menuHelper = inject(MenuHelper);
     private readonly tourService = inject(TourService);
+    private readonly sanitizer = inject(DomSanitizer);
     private readonly preguntaFrecuenteDao = inject(PreguntaFrecuenteDao);
+    private readonly videoTutorialDao = inject(VideoTutorialDao);
     authStore = inject(AuthStore);
 
     error = signal('');
 
+    constructor() {
+        effect(() => {
+            const videoSeleccionado = this.videoSeleccionado();
+
+            untracked(() => {
+                if (videoSeleccionado) {
+                    document.body.classList.add('overflow-hidden!');
+                } else {
+                    document.body.classList.remove('overflow-hidden!');
+                }
+            });
+        });
+    }
+
     ngOnInit(): void {
         this.obtenerPreguntasFrecuentes();
+        this.obtenerVideosTutoriales();
+    }
+
+    ngOnDestroy(): void {
+        document.body.classList.remove('overflow-hidden!');
     }
 
     cargandoPreguntasFrecuentes = signal<boolean>(false);
@@ -62,6 +86,95 @@ export class Ayuda implements OnInit {
                 this.cargandoPreguntasFrecuentes.set(false);
             });
     }
+
+    cargandoVideosTutoriales = signal<boolean>(false);
+    videosTutoriales = signal<SalVideoTutorialHabilitado[]>([]);
+
+    obtenerVideosTutoriales() {
+        this.cargandoVideosTutoriales.set(true);
+        this.videosTutoriales.set([]);
+
+        this.videoTutorialDao
+            .obtenerHabilitados()
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe({
+                next: (res) => {
+                    const sorted = res.sort((a, b) => a.orden - b.orden);
+                    this.videosTutoriales.set(sorted);
+                },
+                error: (err) => {
+                    console.error('Error al obtener videos tutoriales', err);
+                    this.error.set(getErrorMessage(err) ?? 'Error al obtener videos tutoriales');
+                },
+            })
+            .add(() => {
+                this.cargandoVideosTutoriales.set(false);
+            });
+    }
+    datosVideosTutoriales = computed(() => {
+        return this.videosTutoriales().map((video) => {
+            const youtube = this.obtenerYoutubeData(video.url);
+
+            return {
+                titulo: video.titulo,
+                descripcion: video.descripcion,
+                url: video.url,
+                ...(youtube ?? {}),
+            };
+        });
+    });
+
+    private obtenerYoutubeVideoId(urlString: string): string | null {
+        try {
+            const url = new URL(urlString);
+
+            switch (url.hostname) {
+                case 'youtube.com':
+                case 'www.youtube.com':
+                case 'm.youtube.com':
+                    return url.searchParams.get('v');
+
+                case 'youtu.be':
+                    return url.pathname.slice(1);
+
+                default:
+                    return null;
+            }
+        } catch {
+            return null;
+        }
+    }
+
+    private obtenerYoutubeData(urlString: string) {
+        const videoId = this.obtenerYoutubeVideoId(urlString);
+        if (videoId) {
+            return {
+                youtube: {
+                    embeddedUrl: `https://www.youtube.com/embed/${videoId}?autoplay=1&playsinline=1&rel=0`,
+                    miniaturaUrl: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`,
+                    miniaturaMaxResUrl: `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`,
+                },
+            };
+        }
+
+        return undefined;
+    }
+
+    videoSeleccionado = signal<
+        | {
+              youtube?: { embeddedUrl: string; miniaturaUrl: string; miniaturaMaxResUrl: string } | undefined;
+              titulo: string;
+              descripcion: string | null;
+              url: string;
+          }
+        | undefined
+    >(undefined);
+    safeEmbeddedUrl = computed(() => {
+        if (this.videoSeleccionado()?.youtube?.embeddedUrl)
+            return this.sanitizer.bypassSecurityTrustResourceUrl(this.videoSeleccionado()!.youtube!.embeddedUrl);
+
+        return undefined;
+    });
 
     modulos = signal([
         {
