ac90e940970fe0346cf80675e5b6919ecb14e815
Benjamín <benjamin.diaze@outlook.com>
2026-07-12 18:47:36 -0400

    Se reemplazan suscripciones de negocio-store por resumenSuscripcion.

--- src/app/services/negocio-store.ts
+++ src/app/services/negocio-store.ts
@@ -1,8 +1,7 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { SalNegocio } from '../entities/others/sal-negocio';
-import { getCookie } from '../helpers/cookie-helper';
 import { SalNegocioInformacionUsuario } from '../entities/others/sal-negocio-informacion-usuario';
-import { SalSuscripcion } from '../entities/others/sal-suscripcion';
+import { SalSuscripcionResumen } from '../entities/others/sal-suscripcion-resumen';
 
 @Injectable({
     providedIn: 'root',
@@ -11,31 +10,5 @@ export class NegocioStore {
     negocioSeleccionado = signal<SalNegocio | null>(null);
     negociosUsuario = signal<SalNegocio[]>([]);
     informacionUsuario = signal<SalNegocioInformacionUsuario | null>(null);
-    suscripcionesUsuario = signal<SalSuscripcion[]>([]);
-
-    suscripcionActualUsuario = computed(() => {
-        const suscripciones = this.suscripcionesUsuario();
-
-        // Buscamos la suscripción con mayor expiración...
-        const suscripcionesConFechaExpiracion = suscripciones.filter((s) => s.fechaExpiracion && new Date(s.fechaExpiracion) > new Date());
-        if (suscripcionesConFechaExpiracion.length > 0) {
-            const mayorExpiracion = suscripcionesConFechaExpiracion.reduce((max, actual) =>
-                new Date(actual.fechaExpiracion!) > new Date(max.fechaExpiracion!) ? actual : max,
-            );
-            if (mayorExpiracion) return mayorExpiracion;
-        }
-
-        return null;
-    });
-
-    suscripcionActualGratuita = computed(() => {
-        const suscripcionActual = this.suscripcionActualUsuario();
-        if (!suscripcionActual) return null;
-
-        if (suscripcionActual.precioPlan === 0) {
-            return suscripcionActual;
-        }
-
-        return null;
-    });
+    resumenSuscripcionUsuario = signal<SalSuscripcionResumen | null>(null);
 }
