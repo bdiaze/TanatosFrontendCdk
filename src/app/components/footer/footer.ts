94aefb3819682708794a2384150d0ade1f7ed45b
Benjamín <benjamin.diaze@outlook.com>
2026-08-18 12:07:02 -0400

    Se cambian redes sociales en footer.

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -6,10 +6,11 @@ import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideBadgeCheck, lucideFacebook, lucideInstagram, lucideTwitter } from '@ng-icons/lucide';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { FadeIn } from '@/app/directives/fade-in';
+import { Icon } from '@components/icon/icon';
 
 @Component({
     selector: 'app-footer',
-    imports: [HlmButtonImports, RouterLink, NgIcon, HlmIcon, FadeIn],
+    imports: [HlmButtonImports, RouterLink, NgIcon, HlmIcon, FadeIn, Icon],
     templateUrl: './footer.html',
     styleUrl: './footer.scss',
     providers: [
@@ -22,4 +23,7 @@ import { FadeIn } from '@/app/directives/fade-in';
 })
 export class Footer {
     urlLogo = `${environment.urlImages}/images/logo-blanco.svg`;
+    urlLogoTikTok = `${environment.urlImages}/images/redes-sociales/logo-tiktok.svg`;
+    urlLogoLinkedIn = `${environment.urlImages}/images/redes-sociales/logo-linkedin.svg`;
+    urlLogoYouTube = `${environment.urlImages}/images/redes-sociales/logo-youtube.svg`;
 }
