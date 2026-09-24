2d31842aeed6500885a304e9d22b62655a893b94
Benjamín <benjamin.diaze@outlook.com>
2026-09-24 14:41:01 -0300

    Se quita misión, visión y valores de menu-inicial.

--- src/app/features/tableros/menu-inicial/menu-inicial.ts
+++ src/app/features/tableros/menu-inicial/menu-inicial.ts
@@ -1,6 +1,6 @@
-import { Component, computed, effect, inject, signal, untracked } from '@angular/core';
+import { Component, effect, inject, signal, untracked } from '@angular/core';
 import { HlmItemImports } from '@spartan-ng/helm/item';
-import { HlmH1, HlmH3, HlmH4, HlmP } from '@spartan-ng/helm/typography';
+import { HlmH1, HlmP } from '@spartan-ng/helm/typography';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NegocioStore } from '@/app/services/negocio-store';
 import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
@@ -12,39 +12,12 @@ import { MenuHelper } from '@/app/helpers/menu-helper';
 import { DriveStep } from 'driver.js';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { CampoDinamico, ModalEdicion } from '@/app/components/modal-edicion/modal-edicion';
-import { EditorTexto } from '@/app/components/editor-texto/editor-texto';
-import { NegocioDao } from '@/app/daos/negocio-dao';
-import { EntNegocioMisionVisionValores } from '@/app/entities/others/ent-negocio-mision-vision-valores';
-import { SalNegocio } from '@/app/entities/others/sal-negocio';
-import { getErrorMessage } from '@/app/helpers/error-message';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideChartNoAxesCombined, lucideGoal, lucideTriangleAlert, lucideUserStar, lucideEye, lucideEyeOff } from '@ng-icons/lucide';
-import { HlmIcon } from '@spartan-ng/helm/icon';
 import { HlmAlertImports } from '@spartan-ng/helm/alert';
 
 @Component({
     selector: 'app-menu-inicial',
-    imports: [
-        HlmH1,
-        HlmP,
-        HlmH3,
-        HlmH4,
-        HlmItemImports,
-        RouterLink,
-        HlmSpinnerImports,
-        HlmSkeletonImports,
-        FadeIn,
-        HlmButton,
-        ModalEdicion,
-        EditorTexto,
-        HlmSpinnerImports,
-        NgIcon,
-        HlmIcon,
-        HlmAlertImports,
-    ],
+    imports: [HlmH1, HlmP, HlmItemImports, RouterLink, HlmSpinnerImports, HlmSkeletonImports, FadeIn, HlmButton, HlmSpinnerImports, HlmAlertImports],
     templateUrl: './menu-inicial.html',
-    providers: [provideIcons({ lucideTriangleAlert, lucideGoal, lucideChartNoAxesCombined, lucideUserStar, lucideEye, lucideEyeOff })],
 })
 export class MenuInicial {
     negocioStore = inject(NegocioStore);
@@ -54,91 +27,9 @@ export class MenuInicial {
     private readonly menuHelper = inject(MenuHelper);
     private readonly router = inject(Router);
     private readonly tourService = inject(TourService);
-    private readonly negocioDao = inject(NegocioDao);
 
     private readonly route = inject(ActivatedRoute);
     ayuda = toSignal(this.route.queryParamMap.pipe(map((p) => p.get('ayuda'))));
-
-    camposEdicionMision = signal<CampoDinamico[]>([
-        { llave: 'id', nombre: 'ID', tipo: 'oculto', requerido: true, deshabilitado: true },
-        {
-            llave: 'mision',
-            nombre: 'Misión',
-            tipo: 'editor-texto',
-            requerido: false,
-            deshabilitado: false,
-            toolbarEditarTexto: [['bold', 'italic', 'underline'], [{ header: [2, false] }]],
-            formatsEditarTexto: ['bold', 'italic', 'underline', 'header'],
-        },
-        {
-            llave: 'vision',
-            nombre: 'Visión',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-        {
-            llave: 'valores',
-            nombre: 'Valores',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-    ]);
-
-    camposEdicionVision = signal<CampoDinamico[]>([
-        { llave: 'id', nombre: 'ID', tipo: 'oculto', requerido: true, deshabilitado: true },
-        {
-            llave: 'mision',
-            nombre: 'Misión',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-        {
-            llave: 'vision',
-            nombre: 'Visión',
-            tipo: 'editor-texto',
-            requerido: false,
-            deshabilitado: false,
-            toolbarEditarTexto: [['bold', 'italic', 'underline'], [{ header: [false] }]],
-            formatsEditarTexto: ['bold', 'italic', 'underline', 'header'],
-        },
-        {
-            llave: 'valores',
-            nombre: 'Valores',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-    ]);
-
-    camposEdicionValores = signal<CampoDinamico[]>([
-        { llave: 'id', nombre: 'ID', tipo: 'oculto', requerido: true, deshabilitado: true },
-        {
-            llave: 'mision',
-            nombre: 'Misión',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-        {
-            llave: 'vision',
-            nombre: 'Visión',
-            tipo: 'oculto',
-            requerido: false,
-            deshabilitado: true,
-        },
-        {
-            llave: 'valores',
-            nombre: 'Valores',
-            tipo: 'editor-texto',
-            requerido: false,
-            deshabilitado: false,
-            toolbarEditarTexto: [['bold', 'italic', 'underline'], [{ header: [false] }]],
-            formatsEditarTexto: ['bold', 'italic', 'underline', 'header'],
-        },
-    ]);
 
     constructor() {
         effect(() => {
@@ -149,72 +40,6 @@ export class MenuInicial {
                 }
             });
         });
-    }
-
-    misionMostrar = computed(() => {
-        if (this.ayudaRunning()) {
-            return '<h2>Haz click aquí para declarar <strong>la misión</strong> que mejor represente a tu negocio.</h2>';
-        }
-        return this.negocioSeleccionado()?.mision;
-    });
-
-    showVisionValores = signal<boolean>(false);
-
-    showModalMision = signal<boolean>(false);
-    openModalMision() {
-        this.showModalMision.set(true);
-    }
-    closeModalMision() {
-        this.showModalMision.set(false);
-    }
-
-    showModalVision = signal<boolean>(false);
-    openModalVision() {
-        this.showModalVision.set(true);
-    }
-    closeModalVision() {
-        this.showModalVision.set(false);
-    }
-
-    showModalValores = signal<boolean>(false);
-    openModalValores() {
-        this.showModalValores.set(true);
-    }
-    closeModalValores() {
-        this.showModalValores.set(false);
-    }
-
-    error = signal<string>('');
-    actualizandoMisionVisionValores = signal<boolean>(false);
-    tipoActualizando = signal<null | 'mision' | 'vision' | 'valores'>(null);
-    actualizarMisionVisionValores(entrada: SalNegocio, tipo: 'mision' | 'vision' | 'valores') {
-        this.actualizandoMisionVisionValores.set(true);
-        this.tipoActualizando.set(tipo);
-        this.negocioDao
-            .actualizarMisionVisionValores({
-                id: entrada.id,
-                mision: entrada.mision,
-                vision: entrada.vision,
-                valores: entrada.valores,
-            } as EntNegocioMisionVisionValores)
-            .subscribe({
-                error: (err) => {
-                    console.error('Error al guardar tu misión, visión y valores', err);
-                    this.error.set(getErrorMessage(err) ?? 'Error al guardar tu misión, visión y valores');
-                },
-            })
-            .add(() => {
-                this.negocioDao
-                    .obtenerVigentes()
-                    .subscribe({})
-                    .add(() => {
-                        this.actualizandoMisionVisionValores.set(false);
-                        this.tipoActualizando.set(null);
-                    });
-            });
-        this.closeModalMision();
-        this.closeModalVision();
-        this.closeModalValores();
     }
 
     ayudaRunning = signal<boolean>(false);
@@ -306,23 +131,6 @@ export class MenuInicial {
         steps.push(
             ...[
                 {
-                    element: '#mision',
-                    popover: {
-                        title: 'La misión de tu negocio',
-                        description: 'Acá podrás declarar la razón de ser de tu negocio. Haz click para modificarla.',
-                    },
-                    onHighlightStarted: () => {
-                        this.menuHelper.cerrarMenu();
-                    },
-                },
-                {
-                    element: '#boton-vision-valores',
-                    popover: {
-                        title: 'La visión y los valores',
-                        description: 'Y también podrás declarar la visión y los valores de tu negocio haciendo click aquí.',
-                    },
-                },
-                {
                     element: '#acceso-rapido-mi-calendario',
                     popover: {
                         title: 'Tu calendario',
@@ -363,7 +171,6 @@ export class MenuInicial {
             pasos: steps,
             onFinish: () => {
                 this.ayudaRunning.set(false);
-                this.showVisionValores.set(showVisionValores);
                 this.menuHelper.cerrarMenu();
                 if (this.ayuda() === '1') {
                     this.router.navigate(['/ayuda']);
@@ -378,8 +185,6 @@ export class MenuInicial {
             };
         }
 
-        const showVisionValores = this.showVisionValores();
-        this.showVisionValores.set(false);
         this.ayudaRunning.set(true);
         this.tourService.iniciarTour(config);
     }
