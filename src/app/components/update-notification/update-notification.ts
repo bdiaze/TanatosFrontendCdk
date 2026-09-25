28a5f31962fc3e05a8fdd6d0e6eb70a2a54c1176
Benjamín <benjamin.diaze@outlook.com>
2026-09-25 13:06:45 -0300

    Se añade spinner en update-notification.

--- src/app/components/update-notification/update-notification.ts
+++ src/app/components/update-notification/update-notification.ts
@@ -1,14 +1,15 @@
 import { PwaUpdate } from '@/app/services/pwa-update';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideRefreshCw } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmIcon } from '@spartan-ng/helm/icon';
+import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
 import { HlmH4, HlmP } from '@spartan-ng/helm/typography';
 
 @Component({
     selector: 'app-update-notification',
-    imports: [HlmP, HlmH4, HlmButtonImports, NgIcon, HlmIcon],
+    imports: [HlmP, HlmH4, HlmButtonImports, NgIcon, HlmIcon, HlmSpinnerImports],
     templateUrl: './update-notification.html',
     styleUrl: './update-notification.scss',
     providers: [
@@ -20,7 +21,10 @@ import { HlmH4, HlmP } from '@spartan-ng/helm/typography';
 export class UpdateNotification {
     protected readonly pwaUpdate = inject(PwaUpdate);
 
+    protected readonly ejecutandoUpdate = signal(false);
+
     protected async update(): Promise<void> {
+        this.ejecutandoUpdate.set(true);
         await this.pwaUpdate.activateUpdate();
     }
 }
