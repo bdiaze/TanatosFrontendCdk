3b2364b89ce94aed0dde04ea140f35efb5bd47ad
Benjamín <benjamin.diaze@outlook.com>
2026-09-25 17:10:34 -0300

    Se añade header ngsw-bypass a xhr-http-client.

--- src/app/services/xhr-http-client.ts
+++ src/app/services/xhr-http-client.ts
@@ -1,4 +1,4 @@
-import { HttpBackend, HttpClient } from '@angular/common/http';
+import { HttpBackend, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -11,11 +11,28 @@ export class XhrHttpClient {
         this.http = new HttpClient(backend);
     }
 
-    put<T>(url: string, body: any, options?: any) {
-        return this.http.put<T>(url, body, options);
+    put<T>(url: string, body: any, options: any = {}) {
+        return this.http.put<T>(url, body, {
+            ...options,
+            headers: this.addBypassHeader(options.headers),
+        });
     }
 
-    post<T>(url: string, body: any, options?: any) {
-        return this.http.post<T>(url, body, options);
+    post<T>(url: string, body: any, options: any = {}) {
+        return this.http.post<T>(url, body, {
+            ...options,
+            headers: this.addBypassHeader(options.headers),
+        });
+    }
+
+    private addBypassHeader(headers: HttpHeaders | Record<string, string> | undefined) {
+        if (headers instanceof HttpHeaders) {
+            return headers.set('ngsw-bypass', '');
+        }
+
+        return {
+            ...(headers ?? {}),
+            'ngsw-bypass': '',
+        };
     }
 }
